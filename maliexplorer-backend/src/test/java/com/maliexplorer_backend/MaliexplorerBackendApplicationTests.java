bff59f92f49d92b36473a0e0eb20f8b1e588a6bd
Hamathx999 <hamath.o.diallo18@gmail.com>
2026-09-24 16:55:22 +0000

    First commit for the project !

---
+++ maliexplorer-backend/src/test/java/com/maliexplorer_backend/MaliexplorerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliexplorer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaliexplorerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
